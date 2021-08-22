a78e34cf83cff0e12a63b81e57423f5c6c066b84
Daigo Tsubouchi <36166146+Detsudetsu@users.noreply.github.com>
2021-08-22 17:55:05 +0900

    Add feature to print version

--- tfsp/__main__.py
+++ tfsp/__main__.py
@@ -1,7 +1,12 @@
 from .tag_from_spotify import tfsp
 import fire
+import sys
+from ._version import version
 
 def main():
+    if (any(a in sys.argv[1:] for a in ("-V", "--version"))):
+        print(version)
+        sys.exit()
     fire.Fire(tfsp)
 
 if __name__ == '__main__':
