3fe9674552f40678ad09452d7d5973671f9015bc
Daigo Tsubouchi <36166146+Detsudetsu@users.noreply.github.com>
2021-08-22 22:55:09 +0900

    Add package name to tfsp -V

--- tfsp/__main__.py
+++ tfsp/__main__.py
@@ -3,10 +3,13 @@ import fire
 import sys
 from ._version import version
 
+def print_version(ver):
+    print("tag-from-spotify", ver)
+    sys.exit()
+
 def main():
     if (any(a in sys.argv[1:] for a in ("-V", "--version"))):
-        print(version)
-        sys.exit()
+        print_version(version)
     fire.Fire(tfsp)
 
 if __name__ == '__main__':
